067ec3d968d237f2ee0a635ea2d29b98dac1daa1
gideok Kim <ikid4127@naver.com>
2025-03-27 15:06:04 +0900

    feat(250327): update lesson code for opencv

--- 20250327/opencv_face_detection.py
+++ 20250327/opencv_face_detection.py
@@ -4,6 +4,26 @@ import gradio as gr
 cascade_path = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
 face_cascade = cv2.CascadeClassifier(cascade_path)
 
+haarcascade_files = [
+    "haarcascade_frontalface_default.xml",
+    "haarcascade_eye.xml",
+    "haarcascade_eye_tree_eyeglasses.xml",
+    "haarcascade_frontalcatface.xml",
+    "haarcascade_frontalcatface_extended.xml",
+    "haarcascade_frontalface_alt.xml",
+    "haarcascade_frontalface_alt2.xml",
+    "haarcascade_frontalface_alt_tree.xml",
+    "haarcascade_fullbody.xml",
+    "haarcascade_lefteye_2splits.xml",
+    "haarcascade_license_plate_rus_16stages.xml",
+    "haarcascade_lowerbody.xml",
+    "haarcascade_profileface.xml",
+    "haarcascade_righteye_2splits.xml",
+    "haarcascade_russian_plate_number.xml",
+    "haarcascade_smile.xml",
+    "haarcascade_upperbody.xml"
+]
+
 def detect_faces(image):
     # 이미지 불러오기
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -21,12 +41,19 @@ with gr.Blocks() as demo:
     gr.Markdown("얼굴 검출 모델")
 
     with gr.Row():
+        cascade_name = gr.Dropdown(label="Cascade Name", choices=haarcascade_files, value=haarcascade_files[0])
         webcam_input = gr.Image(sources="webcam", streaming=True, mirror_webcam=False)
         output_image = gr.Image(streaming=True)
 
     def detect_face_from_webcam(image):
         return detect_faces(image)
     
-    webcam_input.stream(fn=detect_face_from_webcam, inputs=webcam_input, outputs=output_image)
+    def change_cascade(cascade_name):
+        global face_cascade
+        cascade_path = cv2.data.haarcascades + cascade_name
+        face_cascade = cv2.CascadeClassifier(cascade_path)
+    
+    webcam_input.stream(fn=detect_face_from_webcam, inputs=[webcam_input], outputs=[output_image])
+    cascade_name.change(fn=change_cascade, inputs=[cascade_name], outputs=[])
 
 demo.launch(share=True)
